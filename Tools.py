a5cddcfe31b9ead49fa1ba8672f2e9339e8c20ab
m0d2r <mineceafjozikrorokcz@gmail.com>
2025-02-25 10:16:21 +0100

    V1.0

--- Tools.py
+++ Tools.py
@@ -1,60 +1,39 @@
 import os
-import psutil
-import platform
+import webbrowser
+import colorsys
 
-# Get GPU name
-def get_gpu_name():
-    system = platform.system()
-    
-    if system == "Windows":
-        try:
-            gpu_name = os.popen('wmic path win32_VideoController get Name').read().strip().split("\n")[1]
-            return gpu_name if gpu_name else "GPU info not available"
-        except Exception:
-            return "GPU info not available"
-    
-    elif system == "Linux":
-        try:
-            gpu_name = os.popen("lspci | grep -i 'VGA'").read().strip()
-            return gpu_name if gpu_name else "GPU info not available"
-        except Exception:
-            return "GPU info not available"
-    
-    elif system == "Darwin":
-        try:
-            gpu_name = os.popen("system_profiler SPDisplaysDataType | grep 'Chipset Model'").read().strip()
-            return gpu_name if gpu_name else "GPU info not available"
-        except Exception:
-            return "GPU info not available"
+def clear_console():
+    if os.name == 'nt':
+        os.system('cls')
 
-    return "GPU info not available"
+    else:
+        os.system('clear')
 
-# Selections
-def selection():
-    print("Welcome to Tools")
-    print("build: 2")
+clear_console()
+
+def Selections():
+    print("Welcome in tools V1.0")
+    print("build: 1")
 
     print("1. Specs")
-    selection = input("Select input: ")
+    print("2. speedtest")
+    print("3. automatic clicker")
+    print("4. color converter")
+    user = input("Select a option\n")
 
-    if selection == "1":
-        print("OS:", platform.system())
-        print("CPU cores:", os.cpu_count())
+    if user == "1":
+        print("specs")
+        print("CPU cores:", os.cpu_count)
 
-        def get_cpu_freq():
-            freq = psutil.cpu_freq()
-            if freq:
-                return f"{freq.max:.2f} MHz"
-            return "CPU Frequency not available"
-        
-        print("Architecture:", platform.processor())
-        print("RAM:", round(psutil.virtual_memory().total / (1024 ** 3), 2), "GB")
-        print("RAM speed:", )
-        if platform.system() == "Windows":
-            ram_speed = os.popen("wmic memorychip get Speed").read().strip().split("\n")[1]
-            print("RAM Speed:", ram_speed, "MHz")
-        else:
-            print("RAM Speed: Not available on this OS")
+    elif user == "2":
+        webbrowser.open_new_tab("https://www.speedtest.net/")
 
-        print("GPU:", get_gpu_name())
-selection()
+    elif user == "3":
+        print("comming soon")
+
+    elif user == "4":
+        print("1. RGB to Hlv")
+
+        user_2 = input("select option")
+
+    clear_console()
